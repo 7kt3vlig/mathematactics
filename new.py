e0b62d43c1d22e2e71dee6c71095a98a729e82de
Peter <p3t3rcr3ations@gmail.com>
2024-12-28 15:01:09 +0000

    first commit

---
+++ new.py
@@ -0,0 +1,17 @@
+import pyautogui as aut
+import numpy
+import cv2 as cv 
+import time 
+import win32 
+
+
+
+def square(y):
+         y * y 
+
+
+for u in range(10):
+        print(f"the square of {u} is {square(u)}")
+
+
+
